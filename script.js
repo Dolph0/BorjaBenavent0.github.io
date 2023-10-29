8c07c8ae5274a27dccbabbc47d41ab9a0c5de900
DelfIn Artiles Celdrán <98411540+Dolph0@users.noreply.github.com>
2023-10-29 18:45:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,53 @@ const arrow = document.getElementById("arrow");
 const videoBackground = document.getElementById("videoBackground");
 const llamar = document.getElementById("llamar");
 const iconoLlamada = document.getElementById("iconoLlamada");
-// const leftArrow = document.getElementById("leftArrow");
-// const rightArrow = document.getElementById("rightArrow");
-// const reelVideo = document.getElementById("videoSocial");
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+const videoSocial = document.getElementById("videoSocial");
 
 
-
-
-
-const videos = ["vid/CAAMWEB.mp4", "vid/GCWEB.mp4", "vid/LOOKINWEB.mov"];
-let currentVideoIndex = 1; // Inicializar con la fuente "vid/GCWEB.MP4"
+// Esta funcion deberia funcionar para ambos reproductores, no lo hace, por ahora tengo esta para videoSpcial
+// y la siguiente para la landing, volver aqui y hacerlo crema despues de lanzar
+// Función para cambiar el video
+function changeVideo(videosArray, videoElement, currentIndex, isSocial) {
+    const videoSource = isSocial ? socialVideos : videosArray;
+    currentIndex = (currentIndex + 1) % videoSource.length;
+    videoElement.src = videoSource[currentIndex];
+    return currentIndex;
+  }
+  
+  // Identificar si estamos en la página principal o secundaria
+  const isMainPage = document.getElementById("arrow") !== null;
+  
+  // Asignar eventos a las flechas izquierda y derecha
+  if (isMainPage) {
+    arrow.addEventListener("click", function() {
+      currentVideoIndex = changeVideo(videos, videoSocial, currentVideoIndex, false);
+    });
+  } else {
+    leftArrow.addEventListener("click", function() {
+      currentVideoIndex = changeVideo(socialVideos, videoSocial, currentVideoIndex, true);
+    });
+  
+    rightArrow.addEventListener("click", function() {
+      currentVideoIndex = changeVideo(socialVideos, videoSocial, currentVideoIndex, true);
+    });
+  }
+  
+  // Aquí defines tus arreglos de videos
+  const videos = ["vid/CAAMWEB.mp4", "vid/GCWEB.mp4", "vid/LOOKINWEB.mov"];
+  const socialVideos = ["vid/ReelWEb.mov", "vid/ReelWeb1.mov", "vid/ReelWeb2.mov"];
+  
+  let currentVideoIndex = 1; // Inicializar con la fuente "vid/GCWEB.MP4"
+  
+// aqui esta comentado porque ya estan definidos arriba, solo descomentar en caso de usar la funcion de mainpage y no la
+// otra
+// const videos = ["vid/CAAMWEB.mp4", "vid/GCWEB.mp4", "vid/LOOKINWEB.mov"];
+// let currentVideoIndex = 1; // Inicializar con la fuente "vid/GCWEB.MP4"
 
 arrow.addEventListener("click", function() {
-    // Cambiar al siguiente video en el array
+    // momentaneo porque asi funciona, idealmente eliminar esto y hacer que la de arriba funcione correctamente para todo
+    // volver a esto mas adelante, despues de lanzar
     currentVideoIndex = (currentVideoIndex + 1) % videos.length;
     videoBackground.src = videos[currentVideoIndex];
 });
@@ -48,22 +82,6 @@ function esTelefonoMovil() {
     });
   }
 
-
-
-
 //   const socialVideos = ["vid/ReelWEb.mov", "vid/ReelWeb1.mov", "vid/ReelWeb2.mov"];
-// let currentVideoIndexSocial = 1;
-
-// rightArrow.addEventListener("click", function() {
-//     // Cambiar al siguiente video en el array
-//     currentVideoIndexSocial = (currentVideoIndexSocial + 1) % socialVideos.length;
-//     reelVideo.src = socialVideos[currentVideoIndexSocial];
-// });
-  
-
-
-
-
-
 
 
