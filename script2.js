0152b58251c4ed4cfc3a8189ed3e6864bb46a063
DelfIn Artiles Celdrán <98411540+Dolph0@users.noreply.github.com>
2023-11-09 16:21:46 +0000

    update

--- script2.js
+++ script2.js
@@ -1,51 +1,26 @@
 const numero = document.getElementById("numero");
 const llamada = document.getElementById("llamada");
-let visible = false; // Declarar la variable visible
+let visible = false; 
 const arrow = document.getElementById("arrow");
 const videoBackground = document.getElementById("videoBackground");
 const llamar = document.getElementById("llamar");
 const iconoLlamada = document.getElementById("iconoLlamada");
-const leftArrow = document.getElementById("leftArrow");
-const rightArrow = document.getElementById("rightArrow");
-const videoSocial = document.getElementById("videoSocial");
 
 
-// Resto del código...
-
-
-
-
-// Identificar si estamos en la página principal o secundaria
-const isMainPage = document.getElementById("arrow") !== null;
-
-// Asignar eventos a las flechas izquierda y derecha
-if (isMainPage) {
-  arrow.addEventListener("click", function() {
-    currentVideoIndex = changeVideo(videos, videoBackground, currentVideoIndex, false);
-  });
-} else {
-  leftArrow.addEventListener("click", function() {
-    currentVideoIndex = changeVideo(socialVideos, videoSocial, currentVideoIndex, true);
-  });
-
-  rightArrow.addEventListener("click", function() {
-    currentVideoIndex = changeVideo(socialVideos, videoSocial, currentVideoIndex, true);
-  });
-}
-
-// Arreglos de videos
+// Arrow
 const videos = ["vid/CAAMWEB.mp4", "vid/GCWEB.mp4", "vid/LOOKINWEB.mov"];
-const socialVideos = ["vid/ReelWEb.mov", "vid/ReelWeb1.mov", "vid/ReelWeb2.mov"];
+let currentVideoIndex = 1;
 
-let currentVideoIndex = 1; // Inicializar con la fuente "vid/GCWEB.MP4"
 
-// Función para cambiar el video
-function changeVideo(videosArray, videoElement, currentIndex, isSocial) {
-  const videoSource = isSocial ? socialVideos : videosArray;
-  currentIndex = (currentIndex + 1) % videoSource.length;
-  videoElement.src = videoSource[currentIndex];
-  return currentIndex;
+if (arrow) {
+  arrow.addEventListener("click", function() {
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    videoBackground.src = videos[currentVideoIndex];
+  });
 }
+
+
+
 
 // Función para hacer que las imágenes se vean a pantalla completa
 function enableFullScreen(imgElement) {
@@ -64,6 +39,8 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Función para verificar si es un teléfono móvil
+
+
 function esTelefonoMovil() {
   return window.matchMedia("(max-width: 767px)").matches;
 }
